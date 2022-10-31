a0e85ca456ade78257b9e89f6d8e7a662aff38ba
Caro1704 <69012209+Caro1704@users.noreply.github.com>
2022-10-31 13:33:06 -0300

    Correccion heroku

--- src/main/java/com/portfolio/AP/Contoller/PersonaController.java
+++ src/main/java/com/portfolio/AP/Contoller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
     @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
- @CrossOrigin(origins = "https://frontendapcaro.web.app")
+ @CrossOrigin(origins = "https://frontendcaro.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
