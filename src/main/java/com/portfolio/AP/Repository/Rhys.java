6920869cb095fbfc4d218958886b6bb214cdb05e
Caro1704 <69012209+Caro1704@users.noreply.github.com>
2022-10-31 11:55:20 -0300

    Primer commit en heroku

---
+++ src/main/java/com/portfolio/AP/Repository/Rhys.java
@@ -0,0 +1,10 @@
+package com.portfolio.AP.Repository;
+
+import com.portfolio.AP.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
---
+++ src/main/java/com/portfolio/AP/Security/Repository/iRolRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.AP.Security.Repository;
+
+import com.portfolio.AP.Security.Entity.Rol;
+import com.portfolio.AP.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
